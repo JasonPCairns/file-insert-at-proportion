09f23e5bcdc3e4dfc5c3c192e2d4c346fdbc30dc
Your Name <youremailaddress@example.com>
2018-10-20 02:36:22 +1300

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+import math
+
+
+def main(file_in, file_out, n):
+    raw_text = bring_files(file_in, file_out)
+    text_out = insertion(raw_text, int(n))
+    text_to_file(file_out, text_out)
+
+
+def bring_files(file_in, file_out):
+    with open(file_in) as f1:
+        with open(file_out) as f2:
+            return f1.read(), f2.read()
+
+
+def insertion(raw_text, n):
+    text_in = raw_text[0]
+    text_out = raw_text[1]
+    split_point = math.floor(n*len(text_out))
+    return text_out[:split_point] + text_in + text_out[split_point:]
+
+
+def text_to_file(file_out, text_out):
+    with open(file_out, 'w') as f:
+        f.write(text_out)
+
+
+main("file_in.txt", "file_out.txt", 0.3)
