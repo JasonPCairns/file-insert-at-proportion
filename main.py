015dc90c5922829e97ac88a2d1f46275970f36f6
Your Name <youremailaddress@example.com>
2018-10-20 11:47:58 +1300

    added comments

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ def main(file_in, file_out, n):
 
 
 def bring_files(file_in, file_out):
+    """bring files into program"""
     with open(file_in) as f1:
         with open(file_out) as f2:
             return f1.read(), f2.read()
 
 
 def insertion(raw_text, n):
+    """insert raw text of file_in to some proportion of raw text of file_out"""
     text_in = raw_text[0]
     text_out = raw_text[1]
     split_point = math.floor(n*len(text_out))
@@ -21,8 +23,6 @@ def insertion(raw_text, n):
 
 
 def text_to_file(file_out, text_out):
+    """overwrite file_out"""
     with open(file_out, 'w') as f:
         f.write(text_out)
-
-
-main("file_in.txt", "file_out.txt", 0.3)
