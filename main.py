3f0e9838ea3b40f9558ee5432a556d4e4e5d0f62
Your Name <youremailaddress@example.com>
2018-10-20 17:00:28 +1300

    added cases

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+"""
+Takes as command line arguments file1 file2 n
+0<n<1
+Inserts the contents of file1 into file2 n-way through file2
+"""
 import math
+import sys
 
 
-def main(file_in, file_out, n):
+def main():
+    file_in = sys.argv[1]
+    file_out = sys.argv[2]
+    n = sys.argv[3]
+    if not(0 < n < 1):
+        raise ValueError("n must be between 0 and 1 inclusive")
     raw_text = bring_files(file_in, file_out)
     text_out = insertion(raw_text, int(n))
     text_to_file(file_out, text_out)
@@ -19,10 +30,19 @@ def insertion(raw_text, n):
     text_in = raw_text[0]
     text_out = raw_text[1]
     split_point = math.floor(n*len(text_out))
-    return text_out[:split_point] + text_in + text_out[split_point:]
+    if split_point == 0:
+        return text_in + text_out
+    elif split_point == 1:
+        return text_out + text_in
+    else:
+        return text_out[:split_point] + text_in + text_out[split_point:]
 
 
 def text_to_file(file_out, text_out):
     """overwrite file_out"""
     with open(file_out, 'w') as f:
         f.write(text_out)
+
+
+if __name__ == '__main__':
+    main()
